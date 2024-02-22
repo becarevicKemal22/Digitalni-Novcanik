2baf25b903820309b49700206b2402bad4550db8
Kemal Bećarević <116683338+becarevicKemal22@users.noreply.github.com>
2024-02-22 16:29:37 +0100

    dodano polje na migraciju

--- database/migrations/2024_02_22_151557_create_transactions_table.php
+++ database/migrations/2024_02_22_151557_create_transactions_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
 
             $table->string("naziv");
-
+            $table->string("Test");
             $table->timestamps();
         });
     }
